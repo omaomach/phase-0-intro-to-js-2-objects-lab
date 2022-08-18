2d1e0e69c1022bb8eae54c9c6f7254f9207c0441
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-19 00:09:35 +0300

    completed phase-0 into to js-2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Joash',
+    streetAddress: 'Kiambu',
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+const newObj = updateEmployeeWithKeyAndValue(employee,'name','Sam')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value) {
+    obj[key] = value;
+    return obj;
+}
+const newObj2 = destructivelyUpdateEmployeeWithKeyAndValue(employee,'streetAddress','11 Broadway');
+
+function deleteFromEmployeeByKey(obj,key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+const newObj3 = deleteFromEmployeeByKey(employee,'name');
+console.log(newObj3);
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete obj[key];
+    return obj;
+}
+const newObj4 = destructivelyDeleteFromEmployeeByKey(employee,'name');
+console.log(newObj4);
+
